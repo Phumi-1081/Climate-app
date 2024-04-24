d9df3ac19409b81460202e69f09be229d410532b
Phumi-1081 <167133988+Phumi-1081@users.noreply.github.com>
2024-04-24 16:38:08 +0200

    linked javascript file to HTML, then added basic search engine functionalities

---
+++ index.js
@@ -0,0 +1,8 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-input");
+  let cityElement = document.querySelector("#app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
